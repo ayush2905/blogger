f1067b23b8488394bc17819c990b5f025ef5a859
ayush2905 <ayushagrawal2905@gmail.com>
2024-09-28 05:22:20 +0530

    index page, delete and update handlers

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,43 @@ async fn new(
 }
 
 #[get("/")]
-async fn list() -> {
-    
+async fn list(
+    req: HttpRequest,
+    data: web::Data<AppState>,
+    opt_flash: Option<actix_flash::Message<FlashData>>,
+) -> Result<HttpResponse, Error> {
+    let template = &data.templates;
+    let conn = &data.conn;
+
+    // get params
+    let params = web::Query::<Params>::from_query(req.query_string()).unwrap();
+
+    let page = params.page.unwrap_or(1);
+    let posts_per_page = params.posts_per_page.unwrap_or(DEFAULT_POSTS_PER_PAGE);
+    let paginator = Post::find()
+        .order_by_asc(post::Column::Id)
+        .paginate(conn, posts_per_page.try_into().unwrap());
+    let num_pages = paginator.num_pages().await.ok().unwrap();
+
+    let posts = paginator
+        .fetch_page((page - 1).try_into().unwrap())
+        .await
+        .expect("could not retrieve posts");
+    let mut ctx = tera::Context::new();
+    ctx.insert("posts", &posts);
+    ctx.insert("page", &page);
+    ctx.insert("posts_per_page", &posts_per_page);
+    ctx.insert("num_pages", &num_pages);
+
+    if let Some(flash) = opt_flash {
+        let flash_inner = flash.into_inner();
+        ctx.insert("flash", &flash_inner);
+    }
+
+    let body = template
+        .render("index.html.tera", &ctx)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
 
 #[get("/{id}")]
@@ -59,11 +94,51 @@ async fn edit(
 }
 
 #[post("/{id}")]
-async fn update()
+async fn update(
+    data: web::Data<AppState>,
+    id: web:Path<i32>,
+    post_form: web:Form<post::Model>
+) -> actix_flash::Response<HttpResponse, FlashData> {
+    let conn = &data.conn;
+    let form = post_form.into_inner();
+
+    post::ActiveModel {
+        id: Set(id.into_inner()),
+        title: Set(form.title.to_owned()),
+        text: Set(form.text.to_owned())
+    }
+    .save(conn)
+    .await
+    .expect("Could not edit post");
+
+    let flash = FlashData {
+        kind: "success".to_owned(),
+        message: "Post successfully updated".to_owned()
+    };
+
+    actix_flash::Response::with_redirect(flash, "/")
+}
 
 #[post("/delete/{id}")]
-async fn delete() -> {
+async fn delete(
+    data: web::Data<AppState>,
+    id: web:Path<i32>,
+) -> actix_flash::Response<HttpResponse, FlashData> {
+    let conn = &data.conn;
+    let post: post::ActiveModel = Post::find_by_id(id.into_inner())
+                                    .one(conn)
+                                    .await
+                                    .unwrap()
+                                    .unwrap()
+                                    .into();
+    post.delete(conn).await.unwrap();
 
+    let flash = FlashData {
+        kind: "success".to_owned(),
+        message: "Post successfully deleted".to_owned()
+    };
+
+    actix_flash::Response::with_redirect(flash, "/")
 }
 
 #[post("/")]
